5a19d82c09e056f9323f61b1694943cc488dd332
Pedro Martinez <pmartinez752@unab.edu.co>
2023-09-19 18:21:00 -0500

    Commit inicial

---
+++ seccion2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seccion 2 </title>
+</head>
+<body>
+    <h1>FIESTA EN BUCARAMANGA</h1>
+    <h2>Integrantes aqui</h2>
+    <form action="Guardar totales" method="get">
+    
+    Ingresos:
+    <input type="text" name="totales" id="01">
+    </form>
+
+    <h3>
+    <?php
+
+        $suma=0;
+        $total=$suma++;
+        print_r($_GET)
+
+    ?>
+    </h3>
+    
+</body>
+</html>
+
