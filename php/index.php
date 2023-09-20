956201c1c606f56aa861c74bc92d2f8e355038b4
AortizVale <aortiz449@unab.edu.co>
2023-09-19 19:17:48 -0500

    Index completo

---
+++ php/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+        }
+
+        .bg {
+            /* The image used */
+            background-image: url("../Images/background.jpg");
+
+            /* Full height */
+            height: 100%;
+
+            /* Center and scale the image nicely */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+    <title>Bootstrap Example</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<body>
+    <nav style="background-color: grey;" class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid bg-black">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
+                    <li class="nav-item" style="background-color: white;">
+                        <a class="nav-link active" aria-current="page" href="#">Menú Principal</a>
+                    </li>
+                    <li class="nav-item" style="background-color: white;">
+                        <a class="nav-link" href="seccion1.php">Cifrando Dígitos</a>
+                    </li>
+                    <li class="nav-item" style="background-color: white;">
+                        <a class="nav-link" href="seccion2.php">Fiesta Bucaramanga</a>
+                    </li>
+                    <li class="nav-item" style="background-color: white;">
+                        <a class="nav-link" href="seccion4.php">Primos que inician en 1</a>
+                    </li>
+                    <li class="nav-item" style="background-color: white;">
+                        <a class="nav-link" href="seccion5.php">Números cubifinitos</a>
+                    </li>
+                    <li class="nav-item" style="background-color: white;">
+                        <a class="nav-link" href="seccion6.php">Números amigos</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    
+    <div class="bg"><br>
+    <h1 style="color: white;">Ejemplos de programación</h1>
+    <br>
+    <h2 style="color: white;">Universidad Autónoma de Bucaramanga</h2>
+    <ul style="color: white;">
+        <li>Pedro Martínez</li>
+        <li>David Ortegón</li>
+        <li>Juan Moreno</li>
+        <li>Andrés Amado</li>
+        <li>Andrés Ortiz</li>
+    </ul>
+</div>
+
+</body>
+
+</html>
